eda72138f2b2fd0096715c233c9874e4b63c765b
Bot Papi <botfi.app@gmail.com>
2024-01-16 03:12:10 +0000

    Copy Address BugFix

--- src/components/header/WalletSelect.vue
+++ src/components/header/WalletSelect.vue
@@ -3,11 +3,13 @@ import { useWalletStore } from '../../store/walletStore';
 import Utils from '../../classes/Utils';
 import Avatar from '../common/Avatar.vue';
 import { onBeforeMount, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 const props = defineProps({
     btnClass: { type: String, default: ''}
 })
 
+const router = useRouter()
 const walletStore = useWalletStore()
 const { activeWallet } = walletStore
 
@@ -18,17 +20,22 @@ onBeforeMount(() => {
         btnClass.value = props.btnClass
     }
 })
+
+const openWallets = () => {
+    router.push(`/wallet/addresses?r=${Utils.getUriPath()}`)
+}
 </script>
 <template>
     <div class="w-full">
         <button :class="`btn py-2 d-block addr-select-btn w-full ${btnClass}`" 
             v-if="activeWallet"
             data-bs-toggle="modal" 
-            @click.prevent="$router.push(`/wallet/addresses?r=${Utils.getUriPath()}`)"
         >
             <div class="d-flex align-items-center justify-content-between fs-16">
 
-                <div class="d-flex align-items-center mw-50vw">
+                <div @click.prevent="openWallets"
+                    class="d-flex align-items-center mw-50vw"
+                >
                     <Avatar 
                         :name="activeWallet.address" 
                         :square="true" 
@@ -41,10 +48,26 @@ onBeforeMount(() => {
                     </div>
                 </div>
 
-                <div class="font-monospace">
+                <div   @click.prevent="openWallets"
+                    class="font-monospace"
+                >
                     {{  Utils.maskAddress(activeWallet.address, 6, 6)  }}
+                </div>
+
+                <div>
+                    <CopyBtn 
+                        :text="activeWallet.address"
+                        btnClasses="btn btn-sm p-0 btn-primary rounded copybtn" 
+                        :showToast="true"
+                        successText="Address Copied"
+                    />
                 </div>
             </div>
         </button>
     </div>
 </template>
+<style scoped>
+.addr-select-btn:hover {
+    background: none !important;
+}
+</style>
